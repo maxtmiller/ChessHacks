eaed227b265baa715f4710d6eb52de69e3fd13f4
mtr26 <mohamed.trapro@gmail.com>
2025-11-15 01:18:44 -0500

    Performative Men.

--- mohamed/process.py
+++ mohamed/process.py
@@ -2,6 +2,7 @@ from preprocessing import preprocess_data, PreProcess
 import os
 import torch
 import argparse
+import pickle
 
 if __name__ == "__main__":
     # Preprocess data
@@ -10,5 +11,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     files = [os.path.join(args.data_dir, f) for f in os.listdir(args.data_dir)]
     x, y, num_classes, move_to_int = preprocess_data(files)
+
+    pickle.dump(move_to_int, open("move_to_int.pkl", "wb"))
+    print(f"Preprocessed {len(files)} files. Number of classes: {num_classes}")
     torch.save(x, "X.pt")
     torch.save(y, "y.pt")
