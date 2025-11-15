a707ae384254c6f2c25430d9404b95bc9328db38
mtr26 <mohamed.trapro@gmail.com>
2025-11-15 13:57:42 -0500

    Added customizable learning rate and model size parameters to training script.

--- mohamed/training.py
+++ mohamed/training.py
@@ -8,9 +8,9 @@ from model import ChessResNet
 import torch.nn as nn
 from preprocessing import preprocess_data, ChessDataset
 from torch.utils.data import DataLoader
-import os
-import pickle
+import argparse
 from huggingface_hub import hf_hub_download
+
 
 def topk_accuracy(output, target, k=3):
     """Compute top-k accuracy"""
@@ -24,6 +24,8 @@ def train(epochs, model, train_loader, val_loader, device, eval_interval=1, lr=1
     """
     MLflow-ready training loop for supervised chess policy network
     """
+    model.train()
+    model = torch.compile(model, mode="reduce-overhead", fullgraph=True, backend="inductor")
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -90,6 +92,13 @@ def train(epochs, model, train_loader, val_loader, device, eval_interval=1, lr=1
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train chess policy network")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--model_size", type=int, default=4, help="Number of residual blocks in the model")
+    args = parser.parse_args()
+
     X_path = hf_hub_download(
         repo_id="Maynx/Xy",
         filename="X.pt",
@@ -117,16 +126,15 @@ if __name__ == "__main__":
     train_dataset = ChessDataset(X_train, y_train)
     val_dataset = ChessDataset(X_val, y_val)
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
 
     # Initialize model
-    model = ChessResNet(num_res_blocks=4, num_moves=1917)
+    model = ChessResNet(num_res_blocks=args.model_size, num_moves=1917)
     print("Model initialized.")
 
     # Train the model
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    train(epochs=10, model=model, train_loader=train_loader, val_loader=val_loader, device="mps")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train(epochs=args.epochs, model=model, train_loader=train_loader, val_loader=val_loader, device=device, lr=args.lr)
 
     torch.save(model.state_dict(), "model/chess_resnet.pth")
