d535b0f5700a52c0d42cdbbc1cf88c03f39a9044
mtr26 <mohamed.trapro@gmail.com>
2025-11-15 18:08:41 -0500

    .

--- mohamed/training.py
+++ mohamed/training.py
@@ -61,7 +61,7 @@ def train(epochs, model, train_loader, val_loader, device, eval_interval=1, lr=1
             with torch.no_grad():
                 for inputs, labels in val_loader:
                     inputs, labels = inputs.to(device), labels.to(device)
-                    with autocast(device_type="cuda", dev=torch.bfloat16):
+                    with autocast(device_type="cuda", dtype=torch.bfloat16):
                         outputs = model(inputs)
                     val_loss += criterion(outputs, labels).item()
 
